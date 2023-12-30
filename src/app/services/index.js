19d37425bc7411f8267f846a3e5aabc647b38e03
pktherock <pktherock00@gmail.com>
2023-12-30 21:56:35 +0530

    fix deployement issue

--- src/app/services/index.js
+++ src/app/services/index.js
@@ -1,6 +1,6 @@
 import authService from "./auth/auth.service";
 import alertService from "./alert.service";
-import productService from "./product/Product.service";
+import productService from "./product/product.service";
 import cartService from "./cart/Cart.service";
 import orderService from "./order/order.service";
 
