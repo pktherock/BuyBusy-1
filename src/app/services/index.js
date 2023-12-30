a93dbe4afd9dbec11b866e73c74d8f25c03ade04
pktherock <pktherock00@gmail.com>
2023-12-30 21:59:10 +0530

    fix deployement cart import typo

--- src/app/services/index.js
+++ src/app/services/index.js
@@ -1,7 +1,7 @@
 import authService from "./auth/auth.service";
 import alertService from "./alert.service";
 import productService from "./product/product.service";
-import cartService from "./cart/Cart.service";
+import cartService from "./cart/cart.service";
 import orderService from "./order/order.service";
 
 export { authService, alertService, productService, cartService, orderService };
